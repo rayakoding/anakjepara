4f1d744a9a6e1509bf8cee78f5b7abf61a2d761e
Rena Sanjaya <84943447+rayakoding@users.noreply.github.com>
2024-09-09 13:15:17 +0700

    Add files via upload

---
+++ app/Http/Controllers/WarningController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Periksa;
+use App\Models\Kehamilan;
+use Illuminate\Http\Request;
+
+class WarningController extends Controller
+{
+    public function index()
+    {
+        $data =[
+            'title' => 'IMT Abnormal',
+            'imt_18' => Kehamilan::where('imt', '<', 18)->get(),
+            'imt_30' => Kehamilan::where('imt', '>', 30)->get()
+        ];
+
+        return view('admin.warning.imt', $data);
+    }
+
+    public function hb()
+    {
+        $data =[
+            'title' => 'HB Abnormal',
+            'hb' => Periksa::where('hb', '>=', 1)->where('hb', '<=', 11)->get(),
+        ];
+
+        return view('admin.warning.hb', $data);
+    }
+
+    public function denyut()
+    {
+        $data =[
+            'title' => 'Denyut Abnormal',
+            'denyut' => Periksa::where('denyut', '>=', 1)->where('denyut', '<=', 120)->get(),
+        ];
+
+        return view('admin.warning.denyut', $data);
+    }
+
+    public function hipertensi()
+    {
+        $data =[
+            'title' => 'Hipertensi',
+            'hipertensi' => Periksa::where('td_a', '>=', 140)->get(),
+        ];
+
+        return view('admin.warning.hipertensi', $data);
+    }
+}
